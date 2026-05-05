58c0d8683b63f59b68ea269f3ddce4198f0a1006
clkhoo5211 <clkhoo@iotatechs.com>
2026-05-05 14:15:19 +0800

    BeRM7PUb

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/kkks-customer-app/sw.js', { scope: '/kkks-customer-app/' })})}
